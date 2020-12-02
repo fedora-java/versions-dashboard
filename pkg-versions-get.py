32ce22b75738f432b08a40c9d820ae3b46b794cf
terminus-brut <marian.koncek@mailbox.org>
2020-12-02 12:54:10 +0100

    Use asynchronous requests

--- pkg-versions-get.py
+++ pkg-versions-get.py
@@ -23,16 +23,18 @@
 #
 # Author: Marian Koncek <mkoncek@redhat.com>
 
+import aiohttp
+import asyncio
 import json
 import koji
 import markdown2
 import os
 import re
-import requests
 import sys
 import time
 
-from concurrent.futures import ThreadPoolExecutor as thread_pool
+from aiohttp.client_exceptions import ServerDisconnectedError
+from concurrent.futures import ThreadPoolExecutor
 
 ################################################################################
 
@@ -107,106 +109,11 @@ def normalize_version(version: str) -> str:
 	
 	return leading + trailing
 
-def retry_response(request, retries, **kwargs):
-	response = None
-	
-	while retries > 0:
-		response = requests.get(request, kwargs)
-		
-		if response.status_code == 200:
-			break
-		
-		retries -= 1
-	
-	return response
-
 def get_packages() -> {str}:
 	ks = koji.ClientSession("https://koji.kjnet.xyz/kojihub")
 	return set([package["package_name"] for package in filter(
 		lambda package: not package["blocked"], ks.listPackages("mbi-f32", inherited = True)
 	)])
-
-def get_upstream_version(package_name: str) -> {str: str}:
-	result = dict()
-	
-	retries = 3
-	package_response = retry_response(
-		f"https://release-monitoring.org/api/v2/packages/?name={package_name}&distribution=Fedora",
-		retries,
-	)
-	
-	if not package_response:
-		raise RuntimeError(f"Upstream package {package_name} not found, number of retries: {retries}")
-	
-	project_items = package_response.json()["items"]
-	
-	if len(project_items) == 0:
-		raise RuntimeError(f"Upstream package {package_name} not found, there is no package under such name")
-	
-	project_name = project_items[0]["project"]
-	
-	project_response = retry_response(
-		f"https://release-monitoring.org/api/v2/projects/?name={project_name}",
-		retries,
-	)
-	
-	if not project_response:
-		raise RuntimeError(f"Upstream project {project_name} not found, number of retries: {retries}")
-	
-	versions = project_response.json()["items"][0]["versions"]
-	
-	result["latest"] = normalize_version(versions[0])
-	
-	# The first version not having a tilde in its normalized string
-	try:
-		result["latest-stable"] = normalize_version(
-			next((v for v in versions if not "~" in normalize_version(v)))
-		)
-	except StopIteration:
-		pass
-	
-	return result
-
-def get_upstream_versions(package_names: [str]) -> {str: {str: str}}:
-	result = dict()
-	
-	futures = list()
-	
-	for package_name in package_names:
-		futures.append(request_pool.submit(get_upstream_version, package_name))
-	
-	for package_name, project_version in zip(package_names, futures):
-		result[package_name] = project_version.result()
-	
-	return result
-
-def get_upstream_versions_cached(package_names: [str]) -> {str: {str: str}}:
-	update_cache = False
-	result = {}
-	time_retrieved_literal = "time-retrieved"
-	
-	if not os.path.exists(upstream_cache_path):
-		update_cache = True
-	else:
-		with open(upstream_cache_path, "r") as cache_file:
-			cache = json.load(cache_file)
-		
-		if time.time() - cache[time_retrieved_literal] > upstream_cache_interval:
-			update_cache = True
-		else:
-			result = cache["packages"]
-	
-	if update_cache:
-		result = get_upstream_versions(package_names)
-		
-		with open(upstream_cache_path, "w") as cache_file:
-			json.dump({
-				time_retrieved_literal: time.time(),
-				"packages": result
-			}, cache_file, indent = 2)
-			cache_file.write("\n")
-	
-	return result
 
 def get_koji_versions(package_names: [str], url: str, tag: str) -> {str : str}:
 	ks = koji.ClientSession(url)
@@ -228,123 +135,193 @@ def get_fedora_versions(package_names: [str], release: str) -> {str: str}:
 def get_mbi_versions(package_names: [str]) -> {str: str}:
 	return get_koji_versions(package_names, "https://koji.kjnet.xyz/kojihub", "mbi-f32")
 
-def get_mbi_bootstrap_packages() -> {str}:
-	result = set()
-	
-	index = 0
-	response = retry_response("https://pagure.io/mbi/stage2/blob/master/f/project", 3)
-	content = response.content.decode("utf-8")
-	pattern = "\"/mbi/stage2/blob/master/f/project/"
-
-	while True:
-		index = content.find(pattern, index)
+async def get_async_data(packages, version_columns):
+	async def get_upstream_version(package_name: str) -> {str: str}:
+		result = dict()
+		request_retries = 5
 		
-		if index == -1:
-			break
+		while request_retries > 0:
+			try:
+				async with session.get(f"https://release-monitoring.org/api/v2/packages/?name={package_name}&distribution=Fedora") as package_response:
+					if not package_response:
+						raise RuntimeError(f"Upstream package {package_name} not found, number of retries: {retries}")
+					
+					project_items = (await package_response.json())["items"]
+					
+					if len(project_items) == 0:
+						raise RuntimeError(f"Upstream package {package_name} not found, there is no package under such name")
+					
+					project_name = project_items[0]["project"]
+				break
+			except ServerDisconnectedError:
+				request_retries -= 1
+				continue
 		
-		index += len(pattern)
+		while request_retries > 0:
+			try:
+				async with session.get(f"https://release-monitoring.org/api/v2/projects/?name={project_name}") as project_response:
+					if not project_response:
+						raise RuntimeError(f"Upstream project {project_name} not found, number of retries: {retries}")
+					
+					versions = (await project_response.json())["items"][0]["versions"]
+					
+					result["latest"] = normalize_version(versions[0])
+					
+					# The first version not having a tilde in its normalized string
+					try:
+						result["latest-stable"] = normalize_version(
+							next((v for v in versions if not "~" in normalize_version(v)))
+						)
+					except StopIteration:
+						pass
+				break
+			except ServerDisconnectedError:
+				request_retries -= 1
+				continue
 		
-		end = content.find(".properties\"", index)
-		if end < content.find(".xml\"", index):
-		        result.add(content[index : end])
+		return result
 	
-	return result
-
-def get_mbi_bootstrap_versions(package_names: {str}) -> {str: str}:
-	result = dict()
+	async def get_upstream_versions(packages):
+		versions = await asyncio.gather(*[get_upstream_version(p) for p in packages])
+		return {p: v for p, v in zip(packages, versions)}
 	
-	futures = list()
-	
-	def get_mbi_bootstrap_version(name: str) -> str:
-		request = retry_response(f"https://pagure.io/mbi/stage2/raw/master/f/project/{name}.properties", 3)
-		content = request.content.decode("utf-8")
+	async def get_upstream_versions_cached(package_names: [str]) -> {str: {str: str}}:
+		update_cache = False
+		result = {}
+		time_retrieved_literal = "time-retrieved"
 		
-		version_str = "version="
-		version_str_end = "\n"
+		if not os.path.exists(upstream_cache_path):
+			update_cache = True
+		else:
+			with open(upstream_cache_path, "r") as cache_file:
+				cache = json.load(cache_file)
+			
+			if time.time() - cache[time_retrieved_literal] > upstream_cache_interval:
+				update_cache = True
+			else:
+				result = cache["packages"]
 		
-		begin = content.find(version_str) + len(version_str)
-		end = content.find(version_str_end, begin)
+		if update_cache:
+			result = get_upstream_versions(package_names)
+			
+			with open(upstream_cache_path, "w") as cache_file:
+				json.dump({
+					time_retrieved_literal: time.time(),
+					"packages": result
+				}, cache_file, indent = 2)
+				cache_file.write("\n")
 		
-		return content[begin : end]
+		return result
 	
-	for package_name in package_names:
-		futures.append(request_pool.submit(get_mbi_bootstrap_version, bootstrap_package_name.get(package_name, package_name)))
-	
-	for package_name, project_version in zip(package_names, futures):
-		result[package_name] = project_version.result()
-	
-	return result
-
-def get_comments() -> {str : {str: str}}:
-	response = retry_response(
-		"https://pagure.io/java-pkg-versions-comments/raw/master/f/comments.md",
-		3,
-		timeout = 7)
-	
-	if response.status_code != 200:
-		raise Exception("Could not obtain comments")
-	
-	result = dict()
-	name = str()
-	comment = None
-
-	for line in response.text.splitlines():
-		# A new package name
-		if line.startswith("#") and not line.startswith("##"):
-			name = line[1:].strip()
-			result[name] = dict()
-			comment = str()
+	async def get_mbi_bootstrap_versions() -> {str: str}:
+		package_names = None
 		
-		# A new tag
-		elif line.startswith("##"):
-			if name:
-				match = re.match("##\\s*(.*?)\\s*:\\s*(.*)", line)
-				result[name][match.group(1)] = match.group(2).rstrip()
+		async def get_mbi_bootstrap_version(name: str) -> str:
+			async with session.get(f"https://raw.githubusercontent.com/fedora-java/javapackages-bootstrap/master/project/{name}.properties") as response:
+				content = (await response.content.read()).decode()
+				result = next(re.finditer(r"^version=(.*)$", content, re.MULTILINE)).group(1)
+				return result
+			
+		async with session.get("https://github.com/fedora-java/javapackages-bootstrap/tree/master/project") as response:
+			content = (await response.content.read()).decode()
+			pattern = r"\"/fedora-java/javapackages-bootstrap/blob/master/project/(.*?)(:?\.xml|\.properties)\""
+			package_names = {name.group(1) for name in re.finditer(pattern, content)}
 		
-		# End of the comment for the current package name
-		elif line.startswith("---") or (line.startswith("#") and not line.startswith("##") and name):
-			if name:
-				match = re.match("<p>(.*)</p>\\s*", markdown2.markdown(comment), re.DOTALL)
-				result[name]["comment"] = match.group(1)
-			name = str()
-			comment = None
+		package_names.remove("mbi")
 		
-		elif name:
-			comment += line
-			comment += "\n"
+		versions = await asyncio.gather(*[get_mbi_bootstrap_version(p) for p in package_names])
+		return {p: v for p, v in zip(package_names, versions)}
 	
-	return result
-
-################################################################################
-# Tests
-
-assert(normalize_version("1.0b3") == "1.0~b3")
-assert(normalize_version("2.5.0-rc1") == "2.5.0~rc1")
-assert(normalize_version("2.0b6") == "2.0~b6")
-assert(normalize_version("2.0.SP1") == "2.0.SP1")
-assert(normalize_version("3_2_12") == "3.2.12")
-assert(normalize_version("1.0-20050927.133100") == "1.0.20050927.133100")
-assert(normalize_version("3.0.1-b11") == "3.0.1~b11")
-assert(normalize_version("5.0.1-b04") == "5.0.1~b04")
-assert(normalize_version("0.11b") == "0.11b")
-assert(normalize_version("1_6_2") == "1.6.2")
-assert(normalize_version("1.0.1.Final") == "1.0.1")
-assert(normalize_version("3.0.0.M1") == "3.0.0~M1")
-assert(normalize_version("6.0-alpha-2") == "6.0~alpha.2")
-assert(normalize_version("4.13-beta-1") == "4.13~beta.1")
-assert(normalize_version("5.5.0-M1") == "5.5.0~M1")
-assert(normalize_version("3.0.0-M2") == "3.0.0~M2")
-assert(normalize_version("3.0.0-M1") == "3.0.0~M1")
-assert(normalize_version("3.0.0-M3") == "3.0.0~M3")
-assert(normalize_version("3.0.0-beta.1") == "3.0.0~beta.1")
-assert(normalize_version("1.0-alpha-2.1") == "1.0~alpha.2.1")
-assert(normalize_version("1.0-alpha-8") == "1.0~alpha.8")
-assert(normalize_version("1.0-alpha-18") == "1.0~alpha.18")
-assert(normalize_version("1.0-alpha-10") == "1.0~alpha.10")
-assert(normalize_version("1.0-beta-7") == "1.0~beta.7")
-assert(normalize_version("1.0-alpha-5") == "1.0~alpha.5")
-assert(normalize_version("2.0-M10") == "2.0~M10")
-assert(normalize_version("7.0.0-beta4") == "7.0.0~beta4")
+	async def get_comments() -> {str : {str: str}}:
+		request_retries = 5
+		
+		while request_retries > 0:
+			try:
+				async with session.get("https://pagure.io/java-pkg-versions-comments/raw/master/f/comments.md") as response:
+					if response.status != 200:
+						raise Exception("Could not obtain comments")
+					
+					result = dict()
+					name = str()
+					comment = None
+					
+					while not response.content.at_eof():
+						line = (await response.content.readline()).decode()
+						
+						# A new package name
+						if line.startswith("#") and not line.startswith("##"):
+							name = line[1:].strip()
+							result[name] = dict()
+							comment = str()
+						
+						# A new tag
+						elif line.startswith("##"):
+							if name:
+								match = re.match("##\\s*(.*?)\\s*:\\s*(.*)", line)
+								result[name][match.group(1)] = match.group(2).rstrip()
+						
+						# End of the comment for the current package name
+						elif line.startswith("---") or (line.startswith("#") and not line.startswith("##") and name):
+							if name:
+								match = re.match("<p>(.*)</p>\\s*", markdown2.markdown(comment), re.DOTALL)
+								result[name]["comment"] = match.group(1)
+							name = str()
+							comment = None
+						
+						elif name:
+							comment += line
+							comment += "\n"
+				break
+			except ServerDisconnectedError:
+				request_retries -= 1
+				continue
+		
+		return result
+	
+	async with aiohttp.ClientSession() as session:
+		with ThreadPoolExecutor(max_workers = 16) as executor:
+			futures = []
+			
+			for release in version_columns["fedora"]:
+				futures.append(executor.submit(get_fedora_versions, packages, release))
+			
+			futures.append(executor.submit(get_mbi_versions, packages))
+			
+			####################################################################
+			
+			jp_bp, upstream, comments = await asyncio.gather(*[
+				get_mbi_bootstrap_versions(),
+				get_upstream_versions_cached(packages),
+				get_comments(),
+			])
+			
+			fedoras = {version_columns["fedora"][i]: futures[i].result()
+				for i in range(len(version_columns["fedora"]))
+			}
+			
+			mbi = futures[len(version_columns["fedora"])].result()
+			
+			####################################################################
+			
+			result = {package: {
+				"fedora": {release: fedoras[release][package]
+					for release in  fedoras.keys()
+				},
+				"mbi": {
+					version_columns["mbi"][0]: jp_bp.get(bootstrap_package_name.get(package, package), ""),
+					version_columns["mbi"][1]: mbi[package],
+				},
+				"upstream": upstream[package],
+				"comments": {},
+			} for package in sorted(packages)}
+			
+			for k, v in comments.items():
+				try:
+					result[k]["comments"] = v
+				except KeyError:
+					print(f"Warning: package \"{package}\" has comments on the page but is not present in Koji")
+			
+			return result
 
 ################################################################################
 # Shared variables
@@ -355,60 +332,26 @@ output_path = os.environ.get("OUT_JSON", "versions.json")
 upstream_cache_interval = 1 * 60 * 60
 upstream_cache_path = os.environ.get("CACHE_FILE", "/tmp/pkg-versions-upstream-cache.json")
 
-request_pool = thread_pool(20)
-
 ################################################################################
 # Main function
 
-result = {pkg: {} for pkg in get_packages()}
-futures = dict()
+packages = get_packages()
+fedora_releases = range(28, 34 + 1)
 
 version_columns = {
-	"fedora": [f"f{i}" for i in range(28, 35)],
-	"mbi": ["mbi-bootstrap", "mbi"],
+	"fedora": [f"f{i}" for i in fedora_releases],
+	"mbi": ["jp-bootstrap", "mbi"],
 }
 
-futures["fedora"] = dict()
-for fedora_version in version_columns["fedora"]:
-	futures["fedora"][fedora_version] = request_pool.submit(get_fedora_versions, result.keys(), fedora_version)
-
-futures["mbi"] = dict()
-mbi_bootstrap = get_mbi_bootstrap_packages()
-futures["mbi"]["mbi-bootstrap"] = request_pool.submit(get_mbi_bootstrap_versions,
-	{name for name in result.keys() if bootstrap_package_name.get(name, name) in mbi_bootstrap})
-
-futures["mbi"]["mbi"] = request_pool.submit(get_mbi_versions, result.keys())
-futures["upstream"] = request_pool.submit(get_upstream_versions_cached, result.keys())
-futures["comments"] = request_pool.submit(get_comments)
-
-for column_name in version_columns.keys():
-	for k, dic in result.items():
-		dic[column_name] = dict()
-	
-	for column_version, versions_future in futures[column_name].items():
-		versions = versions_future.result()
-		for package, version in versions.items():
-			result[package][column_name][column_version] = version
-
-for package, upstream_versions in futures["upstream"].result().items():
-	result[package]["upstream"] = upstream_versions
-
-for package, comments in futures["comments"].result().items():
-	try:
-		result[package]["comments"] = comments
-	except KeyError:
-		print(f"Package {package} has comments on the page but is not present in Koji")
+versions_result = asyncio.run(get_async_data(packages, version_columns))
 
 with open(output_path, "w") as output_file:
 	result = {
 		"time-generated": time.ctime(),
 		"hostname": os.environ.get("HOSTNAME", "local"),
-		"version-columns": {
-			"fedora": [f for f in futures["fedora"]],
-			"mbi": [m for m in futures["mbi"]],
-		},
+		"version-columns": version_columns,
 		"upstream-columns": ["latest", "latest-stable"],
-		"versions": result,
+		"versions": versions_result,
 	}
 	json.dump(result, output_file, indent = 2)
 	output_file.write("\n")
