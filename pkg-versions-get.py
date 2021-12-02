ac50b46811177e597edb3fc0a9dc0aa5d203be59
Mikolaj Izdebski <mizdebsk@redhat.com>
2021-12-02 10:38:40 +0100

    Don't care about Fedora versions older than 34

--- pkg-versions-get.py
+++ pkg-versions-get.py
@@ -161,7 +161,7 @@ request_pool = thread_pool(32)
 output_path = os.environ.get("OUT_JSON", "versions.json")
 
 futures = {
-    "fedora": {"f" + str(v): None for v in range(28, 36 + 1)},
+    "fedora": {"f" + str(v): None for v in range(34, 36 + 1)},
     "upstream": {},
     "jp-bootstrap": {}
 }
