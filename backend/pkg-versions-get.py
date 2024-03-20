6fca53e01e22fd054fa2d30776f5609a9cfe6ced
Mikolaj Izdebski <mizdebsk@redhat.com>
2024-03-20 12:07:40 +0100

    Update Fedora versions

--- backend/pkg-versions-get.py
+++ backend/pkg-versions-get.py
@@ -161,7 +161,7 @@ request_pool = thread_pool(32)
 output_path = os.environ.get("OUT_JSON", "versions.json")
 
 futures = {
-    "fedora": {"f" + str(v): None for v in range(36, 40 + 1)},
+    "fedora": {"f" + str(v): None for v in range(38, 41 + 1)},
     "upstream": {},
     "jp-bootstrap": {}
 }
