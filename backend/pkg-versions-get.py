807e9d955646f094fc9aeeedacfc4b575e429e4e
Mikolaj Izdebski <mizdebsk@redhat.com>
2024-08-20 14:21:03 +0200

    Bump Fedora versions

--- backend/pkg-versions-get.py
+++ backend/pkg-versions-get.py
@@ -153,7 +153,7 @@ request_pool = thread_pool(8)
 output_path = os.environ.get("OUT_JSON", "versions.json")
 
 futures = {
-    "fedora": {"f" + str(v): None for v in range(38, 41 + 1)},
+    "fedora": {"f" + str(v): None for v in range(40, 42 + 1)},
     "upstream": {},
     "jp-bootstrap": {}
 }
